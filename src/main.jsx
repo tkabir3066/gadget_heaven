21008d794f0324c422eb9dad4f319e3176cbebe7
tkabir3066 <tutulkabir@gmail.com>
2025-05-24 11:52:46 +0530

    Some Modification

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,65 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root.jsx";
+import Error from "./Components/Error/Error.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+import HomePage from "./Components/HomePage/HomePage.jsx";
+import Statistics from "./Components/Statistics/Statistics.jsx";
+import ProductDetails from "./Components/ProductDetails/ProductDetails.jsx";
+import { CartProvider } from "./Components/context/CartContext.jsx";
+import { ToastContainer } from "react-toastify";
+import Contact from "./Components/Contact/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "stats",
+        element: <Statistics />,
+        loader: () => fetch("/product.json"),
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "products/:product",
+        element: <ProductDetails />,
+        loader: () => fetch("/product.json"),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CartProvider>
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </CartProvider>
+  </StrictMode>
+);
